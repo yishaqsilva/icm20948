e128682da6400fa9d31ea50e31f111ac310ef926
yishaqsilva <yishaqsilva@gmail.com>
2026-05-05 13:16:43 +0100

    fix bug

--- bind.cpp
+++ bind.cpp
@@ -1,7 +1,7 @@
 #include "icm20948.h"
 #include "pybind11/pybind11.h"
 
-PYBIND11_MODULE(icm29048, m, pybind11::mod_gil_not_used()) {
+PYBIND11_MODULE(icm20948, m, pybind11::mod_gil_not_used()) {
     pybind11::class_<icm20948>(m, "icm20948")
         .def(pybind11::init<const char*, uint8_t>())
         .def("read_byte", &icm20948::read_byte)
