8b18d4e1254e1597d84eb887b0bb03d825f00437
yishaqsilva <yishaqsilva@gmail.com>
2026-05-06 09:39:42 +0100

    fix bank setting issue

--- icm20948.cpp
+++ icm20948.cpp
@@ -78,10 +78,11 @@ void icm20948::set_accel_fs_sel(uint8_t fs_sel){
 
 accel_data icm20948::get_accel_data(){
     
-    set_bank(0);
 
     float scale_ranges[4] = {16384.0f, 8192.0f, 4096.0f, 2048.0f};
     float fs = scale_ranges[accel_fs_sel];
+
+    set_bank(0);
 
     accel_data dat = {
         
